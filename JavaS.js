6c5be725c6b56dd71026105c58686071af65e592
Harambee <liam.johnston18@alumni.loyola.ca>
2017-04-03 13:34:08 -0400

    Add files via upload

--- JavaS.js
+++ JavaS.js
@@ -1,11 +1,21 @@
 function login(){
 	var user = "Johnston"
 	var pass = "Liam"
-	var prompt1 = prompt ("Please enter Username");
-	var prompt2= prompt ("Please enter password");
-	if (prompt1 === user) {
-		if(prompt2 === pass){
-			alert("Welcome, Johnston")
+	var prompt1 = prompt("Please enter Username");
+	var prompt2 = prompt("Please enter password");
+	
+	if (prompt1 === user){
+		if (prompt2 === pass){
+			confirm("Welcome "+ user +"")
+			console.log("Your in");
+			document.getElementById("user").innerHTML = "Welcome" + " " + user;
+		}else{
+			alert("Wrong password");
+		}	
+		
+	}else{
+		alert("Wrong username");
+			
 		}
-	}
+		
 }
