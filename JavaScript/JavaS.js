2d4a2c954231f7f6748ff3f13dc172072cf547c5
Harambee <liam.johnston18@alumni.loyola.ca>
2017-05-01 14:01:29 -0400

    Add files via upload

--- JavaScript/JavaS.js
+++ JavaScript/JavaS.js
@@ -1,24 +1,13 @@
 function login(){
-	var user = {Possibility1: "Johnston", Possibility2: "Mancuso", Possibilty3: "Kelly"}; /** This is the code for usernames using an object **/ 
-	var pass = {Poss1: "Liam", Poss2: "Sacha", Poss3: "Will"};	/** This is the code for passwords using an object **/ 
+	var user = ["Johnston", "Mancuso", "Kelly", "Fedorovic"] /** This is the code for usernames using an array **/ 
+	var pass = ["Liam", "Sacha", "Will", "Andre"]	/** This is the code for passwords using an array **/ 
 	var prompt1 = prompt("Please enter Username");
 	var prompt2 = prompt("Please enter password");
-	if (prompt1 === user){
-		if (prompt2 === pass){
-			confirm("Welcome "+ user +"")
-			console.log("You're in");
-			document.getElementById("user").innerHTML = "Welcome" + " " + user; /** This happens when you get loged in **/
-		}else{
-			alert("Wrong password"); /** This is what pops up when the password is incorrectly put in **/
-		}	
-		
-	}else{
-		alert("Wrong username"); /** This is what pops up when the username is incorrectly put in **/
-			
+	
+	for (i = 0; i <= user.length; i++){
+		if (prompt1 === user[i] && prompt2 === pass[i]){
+			alert("Welcome "+ user[i] +" ");
+			document.getElementById("user").innerHTML = "Welcome " + user[i]; /** This happens when you get loged in **/
 		}
-		
-
+	}
 }
-
-
-
