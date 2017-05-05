7663b2005f2e557fdba7f23498e4a5a3af56005f
Harambee <liam.johnston18@alumni.loyola.ca>
2017-05-05 14:22:39 -0400

    Add files via upload

--- JavaScript/JavaS.js
+++ JavaScript/JavaS.js
@@ -1,13 +1,23 @@
 function login(){
 	var user = ["Johnston", "Mancuso", "Kelly", "Fedorovic"] /** This is the code for usernames using an array **/ 
 	var pass = ["Liam", "Sacha", "Will", "Andre"]	/** This is the code for passwords using an array **/ 
-	var prompt1 = prompt("Please enter Username");
-	var prompt2 = prompt("Please enter password");
+	var prompt1 = prompt("Please enter Username")
+	var prompt2 = prompt("Please enter password")
 	
-	for (i = 0; i <= user.length; i++){
-		if (prompt1 === user[i] && prompt2 === pass[i]){
-			alert("Welcome "+ user[i] +" ");
-			document.getElementById("user").innerHTML = "Welcome " + user[i]; /** This happens when you get loged in **/
-		}
-	}
+	for (l = 0; l <= user.length; l++){
+		if (prompt1 === user[l] && prompt2 === pass[l]){
+			alert("Welcome "+ user[l] +" ")
+			document.getElementById("user").innerHTML = "Welcome " + user[l]; /** This happens when you get loged in **/
+		  correct = true
+            break
+        }else{
+            correct = false
+        }
+    }
+    if ( correct === true) {
+        document.getElementById("user").innerHTML = "Welcome" + " " + userArray[l]
+    }
+    else {
+        alert("Wrong Information")
+    }
 }
